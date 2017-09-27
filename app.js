499743f121683676ec8ec6a54fe06dd4432b71a7
FreshOceans <FreshOceans@gmail.com>
2017-09-27 06:59:17 -0400

    integrated bootstrap4, updated css

--- app.js
+++ app.js
@@ -4,19 +4,12 @@ app.controller('mainController', function($scope) {
     $scope.main = {};
     $scope.main.title = "AnagramJS";
 
-    $scope.random = function(a) {
-        return a.sort(function() {
-            return  0.5 - Math.random();
-        });
-    };
-
     $scope.reloadRoute = function() {
         $route.reload();
     };
 
     $scope.phrase = {
         firstPhrase: "AngularJS",
-
         fullAnagram: function() {
             var finalPhrase = [];
             finalPhrase = this.firstPhrase.split('').sort(function() { return 0.5 - Math.random()}).join('');
