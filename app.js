8bd7c7a81c52df45c5089207ffb37fcbfc4640c8
FreshOceans <FreshOceans@gmail.com>
2017-10-11 18:54:38 -0400

    fix #1

--- app.js
+++ app.js
@@ -10,19 +10,16 @@ app.controller('mainController', function($scope) {
     };
 
     // Anagram Creation
-    $scope.phrase = {
-        firstPhrase: "",
-        fullAnagram: function() {
-            var finalPhrase;
-            finalPhrase = this.firstPhrase.split('').sort(function() {
-                return 0.5 - Math.random()
-            }).join('');
+    $scope.$watch('initialPhrase', (newVal, oldVal) => {
+    if (newVal) {
+      // set anagram to randomize
+      $scope.anagram = newVal.split('').sort(function() {
+        return 0.5 - Math.random()
+      }).join('');
+    } else {
+      // empty anagram if word is empty
+      $scope.anagram = "";
+    }
+    });
 
-            return finalPhrase
-
-            // Issue with infinte digest loop lies inside $scope.phrase.fullAnagram's return value.
-            // Create a if condition to solve or divide finalPhrase functions
-            // Ex: $scope.random = function() {return 0.5 - Math.random();};
-        }
-    };
 });
